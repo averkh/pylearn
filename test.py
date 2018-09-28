5f09e43233c5f44f86e95c12042a82fabee5fd3c
averkhoglyad <verkhoglyad@game-insight.com>
2018-09-28 11:11:59 +0300

    2**N.py apleN1.py "two tasks about apples" HelloUsername.py Penguins.py "Static" test.py "warpspace"

---
+++ test.py
@@ -0,0 +1,31 @@
+#print('Hello, World!')
+#print(2+3)
+#print(1, 2, 3, 4, sep=' + ', end='')
+#print(' = ', 1 + 2 + 3 + 4, sep='')
+
+
+#speed = 108
+#time = 12
+#dist = speed * time
+#print(dist)
+
+
+#goodByePhrase = 'Hasta la vista'
+#person = 'baby'
+#print(goodByePhrase + ', ' + person + '!')
+
+#a = int(input())
+#b = int(input())
+#print(a + b)
+
+
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#d = int(input())
+#cost1 = a * 100 + b
+#cost2 = c * 100 + d
+#totalCost = cost1 + cost2
+#print(totalCost // 100, totalCost % 100)
+
+
